d8cd891be0c18f36cc6f3db93d4ab3b2ec3c48d7
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-05 16:42:11 +0800

    first time call a function inside another function

--- day-2/variables/src/main.rs
+++ day-2/variables/src/main.rs
@@ -8,17 +8,25 @@ fn main() {
         println!("The value of x in the inner scope is: {x}");
     }
     println!("The value of x is: {x}");
+
+    tuple_datatype();
+    array_datatype();
+    another_function();
 }
 
 // Tuple 
-fn main() {
+fn tuple_datatype() {
     let tup: (i32, f64, u8) = (500, 6.4, 1);
 }
 
 // Array
 // Array in fixed length in Rust
-fn main() {
+fn array_datatype() {
     let a = [3; 5];
     // same as this
     let a = [3, 3, 3, 3, 3];
+}
+
+fn another_function() {
+    println!("Another function.");
 }
