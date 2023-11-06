4b67553f7528120020ae91dbbe315243cb9f8c72
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-07 07:02:43 +0800

    day-3

---
+++ day-3/README.md
@@ -0,0 +1,1 @@
+Starting Control flow, the next chapter is gonna the ownership, can't wait.
---
+++ day-3/control_flow/src/main.rs
@@ -0,0 +1,55 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
+
+// if condition has to be bool
+// error out because if number isn't a bool format
+// unlike JavaScript, Rust won't auto convert non-boolean to boolean.
+// if has to be in a condition, keep that in mind.
+fn main() {
+    let number = 3;
+
+    if number {
+        println!("number was three");
+    }
+}
+
+// So the right way to write the above code would be
+fn main() {
+    let number = 3;
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+}
+
+// using if in a let
+// cause if is an expression, let is allowed to use at the left.
+fn main() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+}
+
+// guess what happens here, Rust doesn't allow different types within a same condition.
+fn main() {
+    let condition = true;
+
+    let number = if condition { 5 } else { "six" };
+    
+    println!("The value of number is: {number}");
+}
+
+// this is a loop, runs till breaks.
+fn main() {
+    loop {
+        println!("again!");
+    }
+}
